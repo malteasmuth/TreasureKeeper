41894a4eb80fe690b17150c273093a940d1de555
Romeo <ralonsoad@gmail.com>
2023-08-29 22:18:24 +0200

    Routes issues

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get "attack", to: "challenges#player_attack"
   end
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :players, only: %i[index show new create edit update delete]
   # Defines the root path route ("/")
