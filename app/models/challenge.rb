466f76cd0ed9ce47935af0001b4c4aea2ded03fc
Victor Loginov <138363579+thegroosalugg@users.noreply.github.com>
2023-08-29 16:35:59 +0200

    Update challenge.rb
    moved validations to single line. Do not need multiple lines for same validations

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,6 +3,5 @@ class Challenge < ApplicationRecord
   belongs_to :monster
   belongs_to :expense
 
-  validates :name, :description, :end_date, presence: true
-  validates :status, presence: true
+  validates :name, :description, :end_date, :status, presence: true
 end
