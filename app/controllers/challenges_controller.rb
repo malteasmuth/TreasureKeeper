a467e74eb5205bc1376c26dd8399363a3fa7e6a6
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-08-29 16:29:02 +0200

    Implement working attack button to challenge

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -15,7 +15,7 @@ class ChallengesController < ApplicationController
     @current_player = Player.find_by(user_id: current_user)
     @challenge = Challenge.new(challenge_params)
     @challenge.player_id = @current_player.id
-    @challenge.monster_id = Monster.create(creature_type: "dragon").id
+    @challenge.monster_id = Monster.create(creature_type: "dragon", healthpoints: 20, hitpoints: 15).id
     if @challenge.save
       redirect_to challenge_path(@challenge)
     else
@@ -32,11 +32,36 @@ class ChallengesController < ApplicationController
   def delete
   end
 
+  def player_attack
+    @challenge = Challenge.find(params["challenge_id"])
+    @player = Player.find_by(user_id: current_user)
+    @player.healthpoints
+    @player.hitpoints
+    @player.rubies
+
+    @monster = Monster.find(@challenge.monster_id)
+    @monster.healthpoints
+    @monster.hitpoints
+
+    @damage_dealt = calculate_damage
+    @monster.hitpoints -= @damage_dealt
+    @monster.save
+    redirect_to challenge_path(@challenge)
+  end
+
   private
 
   def challenge_params
     params.require(:challenge).permit(:name, :description)
-
   end
 
+  def calculate_damage
+    if rand(1..10) == 1
+      0
+    elsif rand(1..10) == 2
+      @player.hitpoints * rand(1.4..2.0).to_i
+    else
+      @player.hitpoints
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :challenges
+  resources :challenges do
+    get "attack", to: "challenges#player_attack"
+  end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
