392b7f54180332a5ed81caca6c1be21aa41ced4f
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-08-29 16:08:01 +0200

    Fix doubled challenges controller

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -32,11 +32,36 @@ class ChallengesController < ApplicationController
   def delete
   end
 
+  def player_attack
+    @player = Player.where(user_id = currentUser)
+    @player.healthpoints
+    @player.hitpoints
+    @player.rubies
+
+    @monster = @challenge.monster_id
+    @monsters.healthpoints
+    @monster.hitpoints
+
+    @damage_dealt = calculate_damage
+    @monsters.hitpoints -= @damage_dealt
+    @monsters.save
+  end
+
   private
 
   def challenge_params
     params.require(:challenge).permit(:name, :description)
+  end
 
+
+  def calculate_damage
+    if rand(1..10) == 1
+      0
+    elsif rand(1..10) == 2
+      @player.hitpoints * rand(1.4..2.0).to_i
+    else
+      @player.hitpoints
+    end
   end
 
 end
