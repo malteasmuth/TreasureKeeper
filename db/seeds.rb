320417f5c809f04adfc5afb9d450976cb0e3b45d
Romeo <ralonsoad@gmail.com>
2023-08-30 15:51:37 +0200

    seed some monsters

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# require "faker"
+
+
+puts "Cleaning database... "
+Monsters.destroy_all
+
+puts "Creating new monsters....🧌🧌"
+Monster.create([{ hitpoints: "20" }, { healthpoints: "100"}, { image_url: "Treasure Keeper/troll_01.png"}, { challenge_id: "1"} ])
+Monster.create([{ hitpoints: "20" }, { healthpoints: "100"}, { image_url: "Treasure Keeper/troll_02.png"}, { challenge_id: "2"} ])
+Monster.create([{ hitpoints: "20" }, { healthpoints: "100"}, { image_url: "Treasure Keeper/orc_01.png"}, { challenge_id: "3"} ])
+Monster.create([{ hitpoints: "20" }, { healthpoints: "100"}, { image_url: "Treasure Keeper/orc_02.png"}, { challenge_id: "4"} ])
+Monster.create([{ hitpoints: "20" }, { healthpoints: "100"}, { image_url: "Treasure Keeper/dragon_01.png"}, { challenge_id: "5"} ])
+Monster.create([{ hitpoints: "20" }, { healthpoints: "100"}, { image_url: "Treasure Keeper/dragon_02.png"}, { challenge_id: "6"} ])
