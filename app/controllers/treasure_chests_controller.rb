55ae4dc61b1648521ebed6de05ca85b3fe9f8141
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-08-31 16:11:12 +0200

    prepare index for treasure-chest-index

--- app/controllers/treasure_chests_controller.rb
+++ app/controllers/treasure_chests_controller.rb
@@ -1,2 +1,8 @@
 class TreasureChestsController < ApplicationController
+  def index
+    @current_player = Player.find_by(user_id: current_user)
+    @treasure_chests = TreasureChest.where(player_id: @current_player.id)
+  end
+
+  
 end
