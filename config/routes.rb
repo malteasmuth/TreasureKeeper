c03531342027cc9ec702236fb09bb28e8949e51f
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-08-31 17:36:17 +0200

    several fixes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  resources :treasure_chests, only: %i[index]
+  resources :treasure_chests, only: %i[index new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :players, only: %i[index show new create edit update delete]
   # Defines the root path route ("/")
