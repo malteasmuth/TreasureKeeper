4faafeb563d3138fcac24eba8231f8dcf8fa84c6
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-09-01 13:19:43 +0200

    Touched almost everything, also modified seeds

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,6 @@
 class Challenge < ApplicationRecord
   # belongs_to :player
-  # belongs_to :treasure_chest, dependent: :destroy
+  belongs_to :treasure_chest
   has_many :expenses, dependent: :destroy
   has_many :monsters, dependent: :destroy
   # validates :name, :description, :monster, :end_date, :status, presence: true
