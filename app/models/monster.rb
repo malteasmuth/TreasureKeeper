b00ddf3fc17c452b0559c23ecbd9cf4e6a6a8c84
Romeo <ralonsoad@gmail.com>
2023-09-01 15:44:55 +0200

    Monster model step 1

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,14 @@
 class Monster < ApplicationRecord
+  has_one_attached :picture
+
+  def icon_creator
+    case self.avatar_url
+      when "Orc"
+        "orc1.png"
+      when  "Troll"
+        "troll.png"
+      else
+        "dragon1.png"
+      end
+  end
 end
