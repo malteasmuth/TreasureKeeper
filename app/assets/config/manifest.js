7115b35cb6105d9ed31875e4fc094162e41f0e2d
Victor Loginov <tkoe88@zoho.com>
2023-09-06 12:22:58 +0200

    updated stimulus controller

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,4 +4,10 @@
 //= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
+
 //= link chest-slam.mp3
+//= link enrage.mp3
+//= link attack.mp3
+//= link coin-drop.mp3
+//= link coin-spill.mp3
+//= link level-up.mp3
--- app/javascript/controllers/challenges_controller.js
+++ app/javascript/controllers/challenges_controller.js
@@ -1,31 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {
+    url: String,
+    url2: String
+  }
   connect() {
     console.log("Everything is fine");
+    console.log(this.url2Value);
   }
 
-  click(event) {
+  // treasure_chests/show/challenges/show page
+  attack(event) {
     const button = event.currentTarget;
-    const buttons = this.element.querySelectorAll(".button, .enrage-button"); // Select buttons
+    const buttonText = event.currentTarget.innerText
+    const enrageSound = new Audio(this.urlValue);
+    const attackSound = new Audio(this.url2Value);
 
     // Add the shake animation class to buttons
-    buttons.forEach((btn) => {
-      btn.classList.add("shake-animation");
-    });
+
+      button.classList.add("shake-animation");
+
+      if (buttonText === 'Attack!') {
+        attackSound.play()
+      } else {
+      enrageSound.play();
+      }
+
 
     // Prevent the default behavior temporarily
     event.preventDefault();
 
     // After a short delay, remove the shake animation class
     setTimeout(() => {
-      buttons.forEach((btn) => {
-        //sounds
-        const chestSlamSound = new Audio(this.urlValue);
-        chestSlamSound.play();
-        //remove animation
-        btn.classList.remove("shake-animation");
-      });
 
       // Execute the button's original functionality (e.g., navigate or trigger some action)
       window.location.href = button.getAttribute("href"); // Example: Navigate to the link's href
