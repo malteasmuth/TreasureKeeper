467a1dca7f189c1942089b89e5963c795d788604
Romeo <ralonsoad@gmail.com>
2023-09-07 01:33:57 +0200

    Categories step1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_083342) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_205526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_083342) do
     t.bigint "challenge_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["challenge_id"], name: "index_expenses_on_challenge_id"
     t.index ["player_id"], name: "index_expenses_on_player_id"
   end
